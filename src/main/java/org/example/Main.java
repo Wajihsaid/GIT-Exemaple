f1d10ae3173addf50254186e7798a448024229b1
Wajihsaid <wajih.said@ensi-uma.tn>
2025-10-03 11:47:53 +0100

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //Mon message
-        System.out.println("salamo aalykom");
-        System.out.println("Hello, World!");
+        //Mon messa
+        System.out.println("je suis master");
     }
 }
