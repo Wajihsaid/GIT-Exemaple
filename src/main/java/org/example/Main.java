7f5dfbc06e4fcfd15be9d124fbc8eae71d57db0f
Wajihsaid <wajih.said@ensi-uma.tn>
2025-10-03 11:31:30 +0100

    seconde commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        //Mon message
+        System.out.println("salamo aalykom");
         System.out.println("Hello, World!");
     }
 }
